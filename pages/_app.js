14f5bd76ec5e78bebbc520c8b32fda3f74c81f29
Nathan Arthur <nathan@nathanarthur.com>
2020-09-03 12:55:32 -0400

    add basic template styling

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import React from "react";
+import '../styles/styles.scss'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <div className={'template-base'}>
+    <header className={'template-base__header'}><h1>AudioVerse</h1></header>
+    <div className={'template-base__content'}>
+      <Component {...pageProps} />
+    </div>
+  </div>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import React from "react";
 
 export default function Home() {
   return (
