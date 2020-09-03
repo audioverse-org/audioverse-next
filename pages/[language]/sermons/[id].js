0be44ea774e0508008c97a4989a72231d45ef588
Nathan Arthur <nathan@nathanarthur.com>
2020-09-03 14:27:46 -0400

    fix sass module usage

--- pages/[language]/sermons/[id].js
+++ pages/[language]/sermons/[id].js
@@ -1,15 +1,15 @@
-import { useRouter } from 'next/router'
+import {useRouter} from 'next/router'
 import ErrorPage from 'next/error'
 import React from "react";
 // import Layout from '../../../components/layout'
-import { getAllPostsWithSlug as getLatestSermons, getSermon } from '../../../lib/api'
-import './[id].module.scss'
+import {getAllPostsWithSlug as getLatestSermons, getSermon} from '../../../lib/api'
+import styles from './[id].module.scss'
 
-export default function Post({ sermon }) {
+export default function Post({sermon}) {
     const router = useRouter()
 
     if (!router.isFallback && !sermon) {
-        return <ErrorPage statusCode={404} />
+        return <ErrorPage statusCode={404}/>
     }
 
     const imageSrc = sermon.imageWithFallback.url,
@@ -20,36 +20,38 @@ export default function Post({ sermon }) {
             {router.isFallback ? (
                 <h1>Loading…</h1>
             ) : (
-                <>
-                    <div className={'template-sermon'}>
-                        <div className="template-sermon__meta">
-                            {imageSrc ? <img src={imageSrc} alt={imageAlt}/> : null}
-                            <div className="template-sermon__meta-text">
-                                <h1>{sermon.title}</h1>
-                                <ul className={'template-sermon__speakers'}>
-                                    {sermon.persons.map(speaker => {
-                                        return <li>{speaker.name}</li>
-                                    })}
-                                </ul>
-                            </div>
+                <div>
+                    <div className={styles.meta}>
+                        {imageSrc ? <img src={imageSrc} alt={imageAlt}/> : null}
+                        <div>
+                            <h1>{sermon.title}</h1>
+                            <ul className={styles.speakers}>
+                                {sermon.persons.map(speaker => {
+                                    return <li>{speaker.name}</li>
+                                })}
+                            </ul>
                         </div>
-
-                        {sermon.recordingDate ? <p>{(new Date(sermon.recordingDate)).toLocaleDateString()}</p> : null}
-                        {sermon.audioFiles.map(file => {
-                            return <div><audio controls src={file.url} preload={'metadata'}>Your browser doesn't support this player.</audio></div>
-                        })}
-
-                        {sermon.description ? <div dangerouslySetInnerHTML={{__html: sermon.description}} /> : null}
-
-                        Other sermons: ...
                     </div>
-                </>
+
+                    {sermon.recordingDate ? <p>{(new Date(sermon.recordingDate)).toLocaleDateString()}</p> : null}
+                    {sermon.audioFiles.map(file => {
+                        return <div>
+                            <audio controls src={file.url} preload={'metadata'}>Your browser doesn't support this
+                                player.
+                            </audio>
+                        </div>
+                    })}
+
+                    {sermon.description ? <div dangerouslySetInnerHTML={{__html: sermon.description}}/> : null}
+
+                    Other sermons: ...
+                </div>
             )}
         </div>
     )
 }
 
-export async function getStaticProps({ params }) {
+export async function getStaticProps({params}) {
     const sermon = await getSermon(params.id)
 
     return {
