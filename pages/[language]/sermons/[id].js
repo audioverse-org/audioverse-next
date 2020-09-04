8627e55c200b7d3a6fd9b2b041dc7dc55b9ddf7f
Nathan Arthur <nathan@nathanarthur.com>
2020-09-04 09:34:14 -0400

    lint fixes

--- pages/[language]/sermons/[id].js
+++ pages/[language]/sermons/[id].js
@@ -1,26 +1,30 @@
-import React from "react";
-import {getAllPostsWithSlug as getLatestSermons, getSermon} from '../../../lib/api'
-import SermonDetail from "../../../containers/sermon/detail";
+import React from 'react';
 
-export default SermonDetail
+import SermonDetail from '../../../containers/sermon/detail';
+import { getAllPostsWithSlug as getLatestSermons, getSermon } from '../../../lib/api';
 
-export async function getStaticProps({params}) {
-    const sermon = await getSermon(params.id)
+export default SermonDetail;
 
-    return {
-        props: {
-            sermon,
-        },
-        revalidate: 10,
-    }
+export async function getStaticProps({ params }) {
+	const sermon = await getSermon(params.id);
+
+	return {
+		props: {
+			sermon,
+		},
+		revalidate: 10,
+	};
 }
 
 export async function getStaticPaths() {
-    const allPosts = await getLatestSermons()
-    const cutOffDate = new Date('2020-06-01')
+	const allPosts = await getLatestSermons();
+	const cutOffDate = new Date('2020-06-01');
 
-    return {
-        paths: allPosts.nodes.filter(node => new Date(node.recordingDate) > cutOffDate).map((node) => `/en/sermons/${node.id}`) || [],
-        fallback: 'unstable_blocking',
-    }
+	return {
+		paths:
+			allPosts.nodes
+				.filter((node) => new Date(node.recordingDate) > cutOffDate)
+				.map((node) => `/en/sermons/${node.id}`) || [],
+		fallback: 'unstable_blocking',
+	};
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
-import '../styles/globals.css'
-import React from "react";
-import '../styles/styles.scss'
+import '../styles/globals.css';
+import React from 'react';
+import '../styles/styles.scss';
 
 function MyApp({ Component, pageProps }) {
-  return <div className={'template-base'}>
-    <header className={'template-base__header'}><h1>AudioVerse</h1></header>
-    <div className={'template-base__content'}>
-      <Component {...pageProps} />
-    </div>
-  </div>
+	return (
+		<div className={'template-base'}>
+			<header className={'template-base__header'}>
+				<h1>AudioVerse</h1>
+			</header>
+			<div className={'template-base__content'}>
+				<Component {...pageProps} />
+			</div>
+		</div>
+	);
 }
 
-export default MyApp
+export default MyApp;
