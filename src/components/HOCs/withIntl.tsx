8295b89b34913bf4784e2839e524f8aaf5a3e6f0
Nathan Arthur <nathan@nathanarthur.com>
2020-09-30 14:38:18 -0400

    start using translation files

--- src/components/HOCs/withIntl.tsx
+++ src/components/HOCs/withIntl.tsx
@@ -2,19 +2,6 @@ import { useRouter } from 'next/router';
 import React from 'react';
 import { IntlProvider } from 'react-intl';
 
-type Messages = {
-	[langCode: string]: {
-		[key: string]: string;
-	};
-};
-
-const messages: Messages = {
-	en: {},
-	es: {
-		title: 'Testimonios',
-	},
-};
-
 const withIntl = <P extends any>(
 	Component: React.ComponentType<P>
 ): React.ComponentType<P> => {
@@ -25,12 +12,11 @@ const withIntl = <P extends any>(
 			? query.language[0]
 			: query.language || 'en';
 
+		// TODO: Use uriencodecomponent
+		const messages = require(`../../../public/compiled-lang/${language}.json`);
+
 		return (
-			<IntlProvider
-				messages={messages[language]}
-				locale={language}
-				defaultLocale="en"
-			>
+			<IntlProvider messages={messages} locale={language} defaultLocale="en">
 				<Component {...(props as any)} />
 			</IntlProvider>
 		);
