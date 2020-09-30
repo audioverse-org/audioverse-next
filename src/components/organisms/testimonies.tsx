c5d32aa78fa674769abac1a0ff4f69f4e3d7c299
Nathan Arthur <nathan@nathanarthur.com>
2020-09-30 09:48:53 -0400

    fix message extraction

--- src/components/organisms/testimonies.tsx
+++ src/components/organisms/testimonies.tsx
@@ -6,7 +6,11 @@ import withIntl from '@components/HOCs/withIntl';
 const Testimonies = () => {
 	return (
 		<h2>
-			<FormattedMessage id={'title'} defaultMessage={'Testimonies'} />
+			<FormattedMessage
+				id="title"
+				defaultMessage="Testimonies"
+				description="Testimonies slider title"
+			/>
 		</h2>
 	);
 };
