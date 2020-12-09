98d78d62425eed06814a9e9f8924337ba0b1b241
Nathan Arthur <nathan@nathanarthur.com>
2020-12-09 11:52:02 -0500

    readability changes

--- src/components/molecules/favorite.tsx
+++ src/components/molecules/favorite.tsx
@@ -3,14 +3,10 @@ import React from 'react';
 import { useIsFavorited } from '@lib/api';
 
 export default function Favorite({ id }: { id: string }): JSX.Element {
-	const { isFavorited, toggle } = useIsFavorited(id);
+	const { isFavorited, toggleFavorited } = useIsFavorited(id);
 
 	return (
-		<button
-			onClick={async () => {
-				await toggle();
-			}}
-		>
+		<button onClick={toggleFavorited}>
 			{isFavorited ? 'Unfavorite' : 'Favorite'}
 		</button>
 	);
--- src/lib/api/useIsFavorited.ts
+++ src/lib/api/useIsFavorited.ts
@@ -1,38 +1,41 @@
 import { useMutation, useQuery, useQueryCache } from 'react-query';
 
-import { isFavorited } from '@lib/api/isFavorited';
-import { setFavorited } from '@lib/api/setFavorited';
+import { isFavorited as _isFavorited } from '@lib/api/isFavorited';
+import { setFavorited as _setFavorited } from '@lib/api/setFavorited';
 
 export function useIsFavorited(
 	id: string
-): { isFavorited: boolean | undefined; toggle: () => void } {
+): { isFavorited: boolean | undefined; toggleFavorited: () => void } {
 	const cache = useQueryCache();
 	const queryKey = ['isFavorited', id];
 
-	const { data: faved } = useQuery(queryKey, async () => isFavorited(id));
+	const { data: isFavorited } = useQuery(queryKey, async () =>
+		_isFavorited(id)
+	);
 
-	const [toggle] = useMutation(
+	const [toggleFavorited] = useMutation(
 		() => {
 			// TODO: Consider using setFavorited response to update data instead of
 			//  invalidating `isFavorited` query
 
+			// TODO: Figure out how to make this data flow less convoluted
 			// onMutate optimistically changes cached data, so we don't
 			// flip faved since it's already been flipped in onMutate
-			return setFavorited(id, !!faved);
+			return _setFavorited(id, !!isFavorited);
 		},
 		{
 			onMutate: () => {
 				cache.cancelQueries(queryKey);
 
-				const previousFaved = faved;
+				const snapshot = isFavorited;
 
-				cache.setQueryData(queryKey, !faved);
+				cache.setQueryData(queryKey, !isFavorited);
 
-				return () => cache.setQueryData(queryKey, previousFaved);
+				return () => cache.setQueryData(queryKey, snapshot);
 			},
-			onError: (err, options, rollback) => rollback(),
+			onError: (err, variables, rollback) => rollback(),
 		}
 	);
 
-	return { isFavorited: faved, toggle };
+	return { isFavorited, toggleFavorited };
 }
