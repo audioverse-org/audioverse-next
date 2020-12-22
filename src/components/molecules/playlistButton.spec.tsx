e8a8b6f82e9db31d83893b45527742158773c5b0
Nathan Arthur <nathan@nathanarthur.com>
2020-12-22 15:16:07 -0500

    create playlist button component

--- src/components/molecules/playlistButton.spec.tsx
+++ src/components/molecules/playlistButton.spec.tsx
@@ -1,4 +1,5 @@
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import React from 'react';
 
 import PlaylistButton from '@components/molecules/playlistButton';
@@ -8,9 +9,19 @@ describe('playlist button', () => {
 		await render(<PlaylistButton />);
 	});
 
-	it('shows error if user not logged in', () => {});
+	it('shows error if user not logged in', async () => {
+		const { getByText } = await render(<PlaylistButton />);
 
-	// shows error if user not logged in
+		const button = getByText('Add to Playlist');
+
+		userEvent.click(button);
+
+		expect(
+			getByText('You must be logged in to perform this action')
+		).toBeInTheDocument();
+	});
+
+	// does not show error if user logged in
 	// gets user playlists
 	// shows user playlists
 	// adds recording to playlist
--- src/components/molecules/playlistButton.tsx
+++ src/components/molecules/playlistButton.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import ReactTooltip from 'react-tooltip';
 
 export default function PlaylistButton(): JSX.Element {
-	return <>hello world</>;
+	return (
+		<>
+			<button data-tip={'You must be logged in to perform this action'}>
+				Add to Playlist
+			</button>
+			<ReactTooltip event={'click'} />
+		</>
+	);
 }
+
+// TODO: Consider moving ReactTooltip to top-level component + custom test
+//   renderer
