024c4b7b33b76e22b9ea8f163835d7ae582660cb
Nathan Arthur <nathan@nathanarthur.com>
2021-01-18 16:50:56 -0500

    console data

--- src/lib/getIntlMessages.ts
+++ src/lib/getIntlMessages.ts
@@ -1,7 +1,9 @@
 import getLanguageByBaseUrl from '@lib/getLanguageByBaseUrl';
 
-export default function getIntlMessages(language: string): any {
-	const lang = getLanguageByBaseUrl(language, 'en');
+export default function getIntlMessages(languageRoute: string): any {
+	const lang = getLanguageByBaseUrl(languageRoute, 'en');
+
+	console.log(`../../public/compiled-lang/${lang?.base_url}.json`);
 
 	return require(`../../public/compiled-lang/${lang?.base_url}.json`);
 }
