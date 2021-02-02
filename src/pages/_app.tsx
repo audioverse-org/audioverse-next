afd249fb11db5b0390cd4654f00e10ecbeeead79
Nathan Arthur <nathan@nathanarthur.com>
2021-02-02 15:50:55 -0500

    add sharing buttons

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -45,6 +45,11 @@ function MyApp<P>({
 				</Hydrate>
 			</QueryClientProvider>
 			<ToastContainer />
+			{/* Go to www.addthis.com/dashboard to customize your tools */}
+			<script
+				type="text/javascript"
+				src="//s7.addthis.com/js/300/addthis_widget.js#pubid=audioverse"
+			/>
 		</div>
 	);
 }
