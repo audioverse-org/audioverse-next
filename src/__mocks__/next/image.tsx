e8430b85e857225b936d5b3075721fc9af40ada3
Nathan Arthur <nathan@nathanarthur.com>
2021-07-27 13:47:52 -0700

    upgrade nextjs

--- src/__mocks__/next/image.tsx
+++ src/__mocks__/next/image.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Image(props: ImageProps): JSX.Element {
 	/* eslint-disable @next/next/no-img-element */
-	return <img {...props} width={props.width} height={props.height} />;
+	return <img {...(props as any)} width={props.width} height={props.height} />;
 }
 
 export default Image;
