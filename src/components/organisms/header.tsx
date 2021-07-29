4901d6dada15ba93afbc7d3da77cbfda2cbc91af
Nathan Arthur <nathan@nathanarthur.com>
2021-07-29 13:39:45 -0700

    sidebar, link styling

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -2,7 +2,7 @@ import { Button } from '@material-ui/core';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 import Icon from '@components/atoms/icon';
 import LoadingIndicator from '@components/molecules/loadingIndicator';
@@ -14,7 +14,73 @@ import styles from './header.module.scss';
 
 const Header = (): JSX.Element => {
 	const languageRoute = useLanguageRoute();
-	const iconSize = 20;
+	const iconSize = 24;
+	const intl = useIntl();
+
+	const entries: {
+		href?: string;
+		icon: JSX.Element;
+		label: string;
+	}[] = [
+		{
+			href: `/${languageRoute}/discover`,
+			icon: <Icon icon={'search'} size={iconSize} />,
+			label: intl.formatMessage({
+				id: `header__navItemDiscover`,
+				defaultMessage: 'Discover',
+				description: `Header nav link name: Discover`,
+			}),
+		},
+		{
+			icon: <Icon icon={'playlist'} size={iconSize} />,
+			label: intl.formatMessage({
+				id: `header__navItemLibrary`,
+				defaultMessage: 'Library',
+				description: `Header nav link name: Library`,
+			}),
+		},
+		{
+			href: `/${languageRoute}/bibles`,
+			icon: <Icon icon={'bible'} size={iconSize} />,
+			label: intl.formatMessage({
+				id: `header__naveItemBible`,
+				defaultMessage: 'Bible',
+				description: `Header nav link name: Bible`,
+			}),
+		},
+		{
+			icon: <Icon icon={'collections'} size={iconSize} />,
+			label: intl.formatMessage({
+				id: `header__navItemCollections`,
+				defaultMessage: 'Collections',
+				description: `Header nav link name: Collections`,
+			}),
+		},
+		{
+			icon: <Icon icon={'blog'} size={iconSize} />,
+			label: intl.formatMessage({
+				id: `header__navItemBlog`,
+				defaultMessage: 'Blog',
+				description: `Header nav link name: Blog`,
+			}),
+		},
+		{
+			icon: <Icon icon={'settings'} size={iconSize} />,
+			label: intl.formatMessage({
+				id: `header__navItemSettings`,
+				defaultMessage: 'Settings',
+				description: `Header nav link name: Settings`,
+			}),
+		},
+		{
+			icon: <Icon icon={'more'} size={iconSize} />,
+			label: intl.formatMessage({
+				id: `header__navItemMore`,
+				defaultMessage: 'More',
+				description: `Header nav link name: More`,
+			}),
+		},
+	];
 
 	return (
 		<header className={styles.header}>
@@ -31,90 +97,34 @@ const Header = (): JSX.Element => {
 				</Link>
 			</h1>
 			<ul>
-				<li>
-					<Icon icon={'playlist'} size={iconSize} />{' '}
-					<FormattedMessage
-						id={`header__navItemPlaylist`}
-						defaultMessage="Playlist"
-						description={`Header nav link name: Playlist`}
-					/>
-				</li>
-				<li>
-					<Link href={`/${languageRoute}/discover`}>
-						<a>
-							<Icon icon={'search'} size={iconSize} />{' '}
-							<FormattedMessage
-								id={`header__navItemDiscover`}
-								defaultMessage="Discover"
-								description={`Header nav link name: Discover`}
-							/>
-						</a>
-					</Link>
-				</li>
-				<li>
-					<Link href={`/${languageRoute}/bibles`}>
-						<a>
-							<Icon icon={'bible'} size={iconSize} />{' '}
-							<FormattedMessage
-								id={`header__naveItemBible`}
-								defaultMessage="Bible"
-								description={`Header nav link name: Bible`}
-							/>
-						</a>
-					</Link>
-				</li>
-				<li>
-					<Icon icon={'collections'} size={iconSize} />{' '}
-					<FormattedMessage
-						id={`header__navItemCollections`}
-						defaultMessage="Collections"
-						description={`Header nav link name: Collections`}
-					/>
-				</li>
-				<li>
-					<Icon icon={'collections'} size={iconSize} />{' '}
-					<FormattedMessage
-						id={`header__navItemBlog`}
-						defaultMessage="Blog"
-						description={`Header nav link name: Blog`}
-					/>
-				</li>
-				<li>
-					<Icon icon={'collections'} size={iconSize} />{' '}
-					<FormattedMessage
-						id={`header__navItemSettings`}
-						defaultMessage="Settings"
-						description={`Header nav link name: Settings`}
-					/>
-				</li>
-				<li>
-					<Icon icon={'collections'} size={iconSize} />{' '}
-					<FormattedMessage
-						id={`header__navItemMore`}
-						defaultMessage="More"
-						description={`Header nav link name: More`}
-					/>
-				</li>
-				<li>
-					<Button variant={'contained'} color={'primary'}>
-						<FormattedMessage
-							id={`header__donateButtonLabel`}
-							defaultMessage="Donate"
-							description={`Header nav donate button label`}
-						/>
-					</Button>
-				</li>
+				{entries.map((e) => {
+					const inner = (
+						<>
+							<span className={styles.icon}>{e.icon}</span>
+							{e.label}
+						</>
+					);
+
+					if (!e.href) return <li key={e.label}>{inner}</li>;
+
+					return (
+						<li key={e.label}>
+							<Link href={e.href}>
+								<a>{inner}</a>
+							</Link>
+						</li>
+					);
+				})}
 			</ul>
-			<div className={styles.user}>
-				{/* TODO: Make dynamic; hide when user not logged in */}
-				<Image src="/img/ivan.png" width={16} height={16} />{' '}
+
+			<Button variant={'contained'} color={'primary'}>
 				<FormattedMessage
-					id={`header__userNamePlaceholder`}
-					defaultMessage="User Name"
-					description={`User name placeholder`}
-				/>{' '}
-				<Icon icon={'chevron-down'} />
-			</div>
+					id={`header__donateButtonLabel`}
+					defaultMessage="Donate"
+					description={`Header nav donate button label`}
+				/>
+			</Button>
+
 			<LoadingIndicator />
 		</header>
 	);
