e65e891041f74fb884e93e53c2e84e1d610cd331
Matthew Leffler <mattleff@gmail.com>
2021-12-01 10:17:01 -0700

    Collapse mobile header on scroll

--- src/components/templates/andNavigation.tsx
+++ src/components/templates/andNavigation.tsx
@@ -1,7 +1,14 @@
 import clsx from 'clsx';
 import Link from 'next/link';
 import { Router, useRouter } from 'next/router';
-import React, { ReactNode, useContext, useEffect, useState } from 'react';
+import React, {
+	ReactNode,
+	useCallback,
+	useContext,
+	useEffect,
+	useRef,
+	useState,
+} from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import Heading6 from '@components/atoms/heading6';
@@ -21,6 +28,14 @@ import MoreIcon from '../../../public/img/icon-more.svg';
 
 import { PlaybackContext } from './andMiniplayer';
 import styles from './andNavigation.module.scss';
+
+const SUBNAV_HEIGHT = 32;
+const HEADER_TITLE_PADDING_TOP_DIFFERENCE = 8;
+const HEADER_TITLE_PADDING_BOTTOM_DIFFERENCE = 6;
+const COLLAPSING_HEIGHT =
+	SUBNAV_HEIGHT +
+	HEADER_TITLE_PADDING_TOP_DIFFERENCE +
+	HEADER_TITLE_PADDING_BOTTOM_DIFFERENCE;
 
 export default function AndNavigation({
 	children,
@@ -51,12 +66,55 @@ export default function AndNavigation({
 	}, [showingMenu]);
 	const onSearchPage = pathname.includes('/[language]/search');
 
+	const headerTitleRef = useRef<HTMLDivElement>(null);
+	const subnavRef = useRef<HTMLDivElement>(null);
+	const [lastScrollTop, setLastScrollTop] = useState<number>(0);
+	const [headerSlideOffset, setHeaderSlideOffset] = useState<number>(0);
+
+	const listener = useCallback(() => {
+		const isServerSide = typeof window === 'undefined';
+		if (isServerSide || !subnavRef.current || !headerTitleRef.current) {
+			return;
+		}
+
+		const scrollTop = Math.max(-document.body.getBoundingClientRect().top, 0);
+
+		const scrollingUp = scrollTop < lastScrollTop;
+		if (scrollingUp && headerSlideOffset + COLLAPSING_HEIGHT < scrollTop) {
+			setHeaderSlideOffset(Math.max(lastScrollTop - COLLAPSING_HEIGHT, 0));
+		} else if (!scrollingUp && headerSlideOffset > scrollTop) {
+			setHeaderSlideOffset(scrollTop);
+		}
+
+		const transitionProgress =
+			Math.max(Math.min(scrollTop - headerSlideOffset, COLLAPSING_HEIGHT), 0) /
+			COLLAPSING_HEIGHT;
+
+		headerTitleRef.current.style.paddingTop = `${
+			24 - transitionProgress * HEADER_TITLE_PADDING_TOP_DIFFERENCE
+		}px`;
+		headerTitleRef.current.style.paddingBottom = `${
+			22 - transitionProgress * HEADER_TITLE_PADDING_BOTTOM_DIFFERENCE
+		}px`;
+		subnavRef.current.style.top = `calc(100% - ${
+			transitionProgress * SUBNAV_HEIGHT
+		}px)`;
+
+		setLastScrollTop(scrollTop);
+	}, [headerSlideOffset, lastScrollTop]);
+	useEffect(() => {
+		window.addEventListener('scroll', listener);
+		return () => {
+			window.removeEventListener('scroll', listener);
+		};
+	}, [listener]);
+
 	const navigationItems = getNavigationItems(router, languageRoute);
 	const playbackRecording = playbackContext.getRecording();
 	return (
 		<div className={styles.positioner}>
 			<div className={styles.mobileHeader}>
-				<div className={styles.mobileHeaderTitle}>
+				<div className={styles.mobileHeaderTitle} ref={headerTitleRef}>
 					<Header />
 					<Button
 						type="super"
@@ -78,25 +136,27 @@ export default function AndNavigation({
 						/>
 					)}
 				</div>
-				<div className={styles.mobileSubnav}>
-					<div className={styles.mobileSubnavItems}>
-						{navigationItems.slice(0, -2).map((item) => (
-							<Heading6 sans large loose unpadded uppercase key={item.key}>
-								<Link href={item.href as string}>
-									<a>{item.label}</a>
-								</Link>
-							</Heading6>
-						))}
+				<div className={styles.mobileSubnavWrapper} ref={subnavRef}>
+					<div className={styles.mobileSubnav}>
+						<div className={styles.mobileSubnavItems}>
+							{navigationItems.slice(0, -2).map((item) => (
+								<Heading6 sans large loose unpadded uppercase key={item.key}>
+									<Link href={item.href as string}>
+										<a>{item.label}</a>
+									</Link>
+								</Heading6>
+							))}
+						</div>
+						<a
+							className={styles.mobileHeaderMore}
+							onClick={(e) => {
+								e.preventDefault();
+								setShowingMenu(true);
+							}}
+						>
+							<MoreIcon />
+						</a>
 					</div>
-					<a
-						className={styles.mobileHeaderMore}
-						onClick={(e) => {
-							e.preventDefault();
-							setShowingMenu(true);
-						}}
-					>
-						<MoreIcon />
-					</a>
 				</div>
 			</div>
 			<div className={styles.wrapper}>
