1fdc69700fe39b38df3cfc8c22117260a95c80b4
Matthew Leffler <mattleff@gmail.com>
2021-12-22 18:24:01 -0500

    Replace React with Preact in client prod builds

--- next.config.js
+++ next.config.js
@@ -545,6 +545,15 @@ module.exports = withBundleAnalyzer(
 			];
 		},
 		webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+			// Replace React with Preact only in client production build
+			if (!dev && !isServer) {
+				Object.assign(config.resolve.alias, {
+					react: 'preact/compat',
+					'react-dom/test-utils': 'preact/test-utils',
+					'react-dom': 'preact/compat',
+				});
+			}
+
 			config.module.rules.push({
 				test: /\.(graphql|gql)$/,
 				exclude: /node_modules/,
