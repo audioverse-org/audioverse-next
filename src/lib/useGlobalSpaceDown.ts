a31f6c755187c72a7e9581dd13cf47d72810c79e
Just <leonboripgs0818@outlook.com>
2022-04-04 06:43:44 +1000

    fix: space-action to BODY elements

--- src/lib/useGlobalSpaceDown.ts
+++ src/lib/useGlobalSpaceDown.ts
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 
 export default (callBack: () => unknown, disabled?: boolean) => {
 	const handleKeyDown = (event: KeyboardEvent) => {
-		if ((event.target as HTMLElement).tagName == 'INPUT' || event.key != ' ') {
+		if ((event.target as HTMLElement).tagName != 'BODY' || event.key != ' ') {
 			return;
 		}
 		callBack();
