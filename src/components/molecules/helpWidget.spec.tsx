e03ecec6a7ba1b20e12bd57bc9d650d4cb5faf33
Nathan Arthur <nathan@nathanarthur.com>
2022-05-17 07:37:39 -0400

    handles widget close events

--- src/components/molecules/helpWidget.spec.tsx
+++ src/components/molecules/helpWidget.spec.tsx
@@ -1,30 +1,24 @@
-import { screen } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
 
 import HelpWidget from '@components/molecules/helpWidget';
 import { buildRenderer } from '@lib/test/helpers';
 
+jest.mock('next/script');
+
 const renderComponent = buildRenderer(HelpWidget);
-const mockBeacon = jest.fn();
+
+const mockBeacon = window.Beacon as jest.Mock;
 
 describe('help widget', () => {
-	beforeEach(() => {
-		jest.clearAllMocks();
-		window.Beacon = mockBeacon;
-	});
-
-	it('renders button', async () => {
-		await renderComponent();
-
-		expect(screen.getByRole('button')).toBeInTheDocument();
-	});
-
 	it('opens widget on click', async () => {
 		await renderComponent();
 
 		const button = screen.getByRole('button');
 		button.click();
 
-		expect(mockBeacon).toBeCalledWith('open');
+		await waitFor(() => {
+			expect(mockBeacon).toBeCalledWith('open');
+		});
 	});
 
 	it('closes widget on second click', async () => {
@@ -37,13 +31,28 @@ describe('help widget', () => {
 		expect(mockBeacon).toBeCalledWith('close');
 	});
 
-	// it('catches widget close event', async () => {
-	// 	await renderComponent();
-	//
-	// 	const button = screen.getByRole('button');
-	// 	button.click();
-	// 	window.Beacon.mock.calls[0][1]();
-	//
-	// 	expect(mockBeacon).toBeCalledWith('close');
-	// });
+	it('catches widget close event', async () => {
+		await renderComponent();
+
+		const button = screen.getByRole('button');
+		button.click();
+
+		await waitFor(() => {
+			expect(mockBeacon).toBeCalledWith('on', 'close', expect.any(Function));
+		});
+
+		await act(async () => {
+			mockBeacon.mock.calls[1][2]();
+		});
+
+		button.click();
+
+		expect(mockBeacon).not.toBeCalledWith('close');
+	});
+
+	it('initializes beacon', async () => {
+		await renderComponent();
+
+		expect(mockBeacon).toBeCalledWith('init', expect.any(String));
+	});
 });
--- src/components/molecules/helpWidget.tsx
+++ src/components/molecules/helpWidget.tsx
@@ -9,8 +9,16 @@ export default function HelpWidget(): JSX.Element {
 	const [isOpen, setIsOpen] = useState<boolean>(false);
 
 	useEffect(() => {
-		// window.FreshworksWidget('hide', 'launcher');
-	});
+		window.Beacon('init', 'e73e9329-30be-4766-99bb-6bfdd739e316');
+
+		window.Beacon('on', 'close', () => {
+			setIsOpen(false);
+		});
+
+		return () => {
+			window.Beacon('off', 'close');
+		};
+	}, []);
 
 	return (
 		<>
@@ -18,12 +26,6 @@ export default function HelpWidget(): JSX.Element {
 				id="beaconOnLoad"
 				dangerouslySetInnerHTML={{
 					__html: `!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});`,
-				}}
-			/>
-			<Script
-				id="beaconInit"
-				dangerouslySetInnerHTML={{
-					__html: `window.Beacon('init', 'e73e9329-30be-4766-99bb-6bfdd739e316')`,
 				}}
 			/>
 			<Button
