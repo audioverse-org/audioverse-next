0edd56221910370452ca838fbd0159de99bfb2ee
Nathan Arthur <nathan@nathanarthur.com>
2022-06-02 16:38:10 -0400

    position help widget

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
 		project: 'tsconfig.json',
 		sourceType: 'module',
 	},
-	ignorePatterns: ['**/*.js'],
+	ignorePatterns: ['**/*.js', 'src/lib/generated/**/*'],
 	plugins: [
 		'@typescript-eslint/eslint-plugin',
 		'import',
