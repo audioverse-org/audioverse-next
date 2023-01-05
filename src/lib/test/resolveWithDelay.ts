3d2e912b05452d665857837013bc3b0ff9a063e0
Nathan Arthur <nathan@nathanarthur.com>
2023-01-05 13:54:09 -0500

    delete unused test helper

--- src/lib/test/resolveWithDelay.ts
+++
@@ -1,9 +0,0 @@
-import { sleep } from '@lib/sleep';
-
-export function resolveWithDelay(
-	mock: vi.SpyInstance,
-	ms = 50,
-	value: any = undefined
-): void {
-	mock.mockImplementation(() => sleep({ ms, value }));
-}
