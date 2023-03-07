6324fa63a5cb4a8a14dbd05c52aa963d18ed51b2
Nathan Arthur <nathan@nathanarthur.com>
2023-03-07 10:19:51 -0500

    readability

--- src/components/templates/andNavigation.tsx
+++ src/components/templates/andNavigation.tsx
@@ -20,7 +20,7 @@ export default function AndNavigation({
 	} = useRouter();
 	const [term, setTerm] = useState((q as string) || '');
 	const [showingMenu, setShowingMenu] = useState(false);
-	const showSearch =
+	const shouldShowSearch =
 		pathname.includes('/[language]/discover') ||
 		pathname.includes('/[language]/search');
 
@@ -44,7 +44,7 @@ export default function AndNavigation({
 							onChange={(v) => setTerm(v)}
 							className={clsx(
 								styles.searchBox,
-								showSearch && styles.searchShown
+								shouldShowSearch && styles.searchShown
 							)}
 						/>
 						<div>{children}</div>
