512e1ac98b7619f43339ad1591ce00f7c3b99e54
Nathan Arthur <nathan@nathanarthur.com>
2023-04-19 11:54:16 -0400

    remove lodash from bibleBrain

---
+++ src/lib/api/bibleBrain.spec.ts
@@ -0,0 +1,39 @@
+import { GetBibleBookContentDocument } from '../generated/graphql';
+import { buildLoader } from '../test/buildLoader';
+import { getBibleBookChapters } from './bibleBrain';
+import getResponse from './bibleBrain.getResponse';
+
+jest.mock('./bibleBrain.getResponse');
+
+const loadData = buildLoader(GetBibleBookContentDocument, {});
+
+describe('bibleBrain', () => {
+	beforeEach(() => {
+		jest.mocked(getResponse).mockResolvedValue({
+			data: [
+				{
+					book_id: 'GEN',
+					book_name: 'Genesis',
+					chapter_start: 2,
+					path: 'ENGKJV/GEN/2.mp3',
+					duration: 1,
+				},
+				{
+					book_id: 'GEN',
+					book_name: 'Genesis',
+					chapter_start: 1,
+					path: 'ENGKJV/GEN/1.mp3',
+					duration: 1,
+				},
+			],
+		});
+		loadData();
+	});
+
+	it('sorts chapters', async () => {
+		const result = await getBibleBookChapters('ENGKJV', 'OLD', 'GEN');
+
+		expect(result[0].number).toBe(1);
+		expect(result[1].number).toBe(2);
+	});
+});
