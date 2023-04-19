820ca830981771b38406775a6a84a15e4eb9b626
Nathan Arthur <nathan@nathanarthur.com>
2023-04-19 12:09:17 -0400

    remove lodash dep from Image mock

--- __mocks__/next/image.tsx
+++ __mocks__/next/image.tsx
@@ -1,13 +1,14 @@
-import transform from 'lodash/transform';
 import { ImageProps } from 'next/image';
 import React from 'react';
 
 function Image(props: ImageProps): JSX.Element {
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { width, height, className, unoptimized, priority, ...rest } = props;
-	const atts = transform(rest, (result: any, val, key) => {
-		result[key.toLowerCase()] = val;
-	});
+
+	const atts = Object.entries(rest).reduce((acc, [k, v]) => ({
+		...acc,
+		[k.toLowerCase()]: v,
+	}));
 	/* eslint-disable @next/next/no-img-element */
 	return (
 		<img
