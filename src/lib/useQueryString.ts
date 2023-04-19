36769ab165149c01423b763259d573629161ce3c
Nathan Arthur <nathan@nathanarthur.com>
2023-04-19 09:34:53 -0400

    terse useQueryString

--- src/lib/useQueryString.ts
+++ src/lib/useQueryString.ts
@@ -1,12 +1,5 @@
-import get from 'lodash/get';
-
 import useRouterQuery from '@lib/useRouterQuery';
 
 export function useQueryString(key: string): string | undefined {
-	const query = useRouterQuery();
-	const raw = get(query, key);
-
-	if (!raw) return undefined;
-
-	return raw.toString();
+	return useRouterQuery()[key]?.toString();
 }
