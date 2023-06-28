4fb35389c952f4959a915b0c32bcceca9490eb1a
Nathan Arthur <nathan@nathanarthur.com>
2023-06-28 11:33:42 -0400

    discover topics section

---
+++ src/components/molecules/card/__generated__/topic.ts
@@ -0,0 +1,17 @@
+import * as Types from '../../../../__generated__/graphql';
+
+export type CardTopicFragment = { __typename?: 'Topic', id: string | number, title: string, summary: string, duration: number, items: { __typename?: 'TopicItemConnection', aggregate: { __typename?: 'Aggregate', count: number } | null } };
+
+export const CardTopicFragmentDoc = `
+    fragment cardTopic on Topic {
+  id
+  title
+  summary
+  duration
+  items {
+    aggregate {
+      count
+    }
+  }
+}
+    `;
---
+++ src/containers/discover/index.section.topics.tsx
@@ -0,0 +1,3 @@
+export default function Topics(): JSX.Element {
+	return <div>Topics</div>;
+}
