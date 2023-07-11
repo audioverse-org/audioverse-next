d959262d8eb9941412d274eba078616828a9d27f
Nathan Arthur <nathan@nathanarthur.com>
2023-07-11 11:07:10 -0400

    topic card tweaks

--- src/components/molecules/card/__generated__/topic.ts
+++ src/components/molecules/card/__generated__/topic.ts
@@ -1,13 +1,12 @@
 import * as Types from '../../../../__generated__/graphql';
 
-export type CardTopicFragment = { __typename?: 'Topic', id: string | number, title: string, summary: string, duration: number, canonicalPath: string, items: { __typename?: 'TopicItemConnection', nodes: Array<{ __typename?: 'TopicItem', entity: { __typename: 'Recording' } | { __typename: 'Sequence' } }> | null, aggregate: { __typename?: 'Aggregate', count: number } | null } };
+export type CardTopicFragment = { __typename?: 'Topic', id: string | number, title: string, summary: string, canonicalPath: string, items: { __typename?: 'TopicItemConnection', nodes: Array<{ __typename?: 'TopicItem', entity: { __typename: 'Recording' } | { __typename: 'Sequence' } }> | null, aggregate: { __typename?: 'Aggregate', count: number } | null } };
 
 export const CardTopicFragmentDoc = `
     fragment cardTopic on Topic {
   id
   title
   summary
-  duration
   items {
     nodes {
       entity {
