3c86db8b373357f94ff02b2c864a08ae83a48a12
Nathan Arthur <nathan@nathanarthur.com>
2023-09-13 13:47:35 -0400

    formatting

--- src/containers/discover/index.tsx
+++ src/containers/discover/index.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
-import BibleBooks from '~src/components/organisms/cardSlider/section/bibleBooks';
 import BlogPosts from '~components/organisms/cardSlider/section/blogPosts';
 import Conferences from '~components/organisms/cardSlider/section/conferences';
 import FeaturedTeachings from '~components/organisms/cardSlider/section/featuredTeachings';
-import Presenters from '~src/components/organisms/cardSlider/section/presenters';
 import RecentTeachings from '~components/organisms/cardSlider/section/recentTeachings';
 import StorySeasons from '~components/organisms/cardSlider/section/storySeasons';
 import Topics from '~components/organisms/cardSlider/section/topics';
 import TrendingMusic from '~components/organisms/cardSlider/section/trendingMusic';
 import TrendingTeachings from '~components/organisms/cardSlider/section/trendingTeachings';
+import BibleBooks from '~src/components/organisms/cardSlider/section/bibleBooks';
+import Presenters from '~src/components/organisms/cardSlider/section/presenters';
 
 export default function Discover(): JSX.Element {
 	return (
