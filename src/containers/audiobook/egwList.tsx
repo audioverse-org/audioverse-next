de9c48d60f0305b286d42c8810fdc1bcafb1f25f
Okeino McGowan <okeino22@yahoo.com>
2023-10-09 10:30:07 -0400

    small test change to egwList

--- src/containers/audiobook/egwList.tsx
+++ src/containers/audiobook/egwList.tsx
@@ -30,15 +30,12 @@ export function EgwAudiobooksList({
 			makeRoute={(l, i) => root.lang(l).egwbooks.page(i).get()}
 		>
 			{nodes.map((node) => (
-				
 				<CardSequence
 					sequence={node}
 					slim={true}
 					egw={true}
 					key={node.canonicalPath}
 				/>
-				
-			
 			))}
 		</PaginatedCardList>
 	);
