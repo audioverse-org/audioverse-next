6e99a8e43068d409d427c5bd42d462c5d43cae1c
Nathan Arthur <nathan@nathanarthur.com>
2024-05-08 12:28:45 -0400

    extract to en.json

--- scripts/lang-extract.ts
+++ scripts/lang-extract.ts
@@ -12,28 +12,47 @@
 //     --out-file public/lang/en.json \
 //     --format lang-formatter.js
 
+import { extractAndWrite } from '@formatjs/cli-lib';
 import fs from 'fs';
+import glob from 'glob';
 
 console.log('hello world');
 
 type Langs = Record<string, { string: string; comment?: string }>;
-const enLangs: Langs = JSON.parse(
-	fs.readFileSync('./public/lang/en.json').toString()
-);
-const langFiles = fs.readdirSync('./public/lang/');
-const enIds = Object.keys(enLangs);
 
-console.log(enIds);
+async function main() {
+	const extractionOptions = {
+		idInterpolationPattern: '[sha512:contenthash:base64:6]',
+		throws: true,
+		extractSourceLocation: true,
+		outFile: 'public/lang/en.json',
+		format: 'lang-formatter.js',
+	};
 
-langFiles.forEach((langFile) => {
-	if (langFile === 'en.json') return;
-	console.log(langFile);
-	const lang = langFile.split('.')[0];
-	const jsonString = fs.readFileSync(`./public/lang/${langFile}`).toString();
-	const langs: Langs = JSON.parse(jsonString);
-	const ids = Object.keys(langs);
-	const missingIds = enIds.filter((id) => !ids.includes(id));
-	const outDatedIds = ids.filter((id) => !enIds.includes(id));
-	console.log(`Missing ids in ${lang}: ${missingIds.length}`);
-	console.log(`Outdated ids in ${lang}: ${outDatedIds.length}`);
-});
+	const files = glob.sync('src/**/*.ts*', { ignore: ['src/**/*.d.ts*'] });
+
+	await extractAndWrite(files, extractionOptions);
+
+	const enLangs: Langs = JSON.parse(
+		fs.readFileSync(`./public/lang/en.json`).toString()
+	);
+	const langFiles = fs.readdirSync('./public/lang/');
+	const enIds = Object.keys(enLangs);
+
+	console.log(enIds);
+
+	langFiles.forEach((langFile) => {
+		if (langFile === 'en.json') return;
+		console.log(langFile);
+		const lang = langFile.split('.')[0];
+		const jsonString = fs.readFileSync(`./public/lang/${langFile}`).toString();
+		const langs: Langs = JSON.parse(jsonString);
+		const ids = Object.keys(langs);
+		const missingIds = enIds.filter((id) => !ids.includes(id));
+		const outDatedIds = ids.filter((id) => !enIds.includes(id));
+		console.log(`Missing ids in ${lang}: ${missingIds.length}`);
+		console.log(`Outdated ids in ${lang}: ${outDatedIds.length}`);
+	});
+}
+
+main().catch(console.error);
