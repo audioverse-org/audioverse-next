7534f6a82835bcde6ec1cb503a9af2b4583d67c7
Nathan Arthur <nathan@nathanarthur.com>
2024-06-27 10:11:33 -0400

    extract DEFAULT_MEDIA_CONTEXT object

--- src/components/templates/andMediaContext.tsx
+++ src/components/templates/andMediaContext.tsx
@@ -2,7 +2,11 @@ import React, { createContext, ReactNode } from 'react';
 
 export type MediaContextType = object;
 
-export const MediaContext = createContext<MediaContextType>({});
+const DEFAULT_MEDIA_CONTEXT: MediaContextType = {};
+
+export const MediaContext = createContext<MediaContextType>(
+	DEFAULT_MEDIA_CONTEXT
+);
 
 export default function AndMediaContext({ children }: { children: ReactNode }) {
 	return <MediaContext.Provider value={{}}>{children}</MediaContext.Provider>;
