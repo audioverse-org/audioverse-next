c362b32abc851e41e27322edbc6e990f172ce1de
Nathan Arthur <nathan@nathanarthur.com>
2024-11-27 16:31:38 -0500

    work on bible passage navigation

--- src/components/organisms/passageNavigation.tsx
+++ src/components/organisms/passageNavigation.tsx
@@ -1,9 +1,13 @@
 import clsx from 'clsx';
 import React, { useState } from 'react';
 
+import Link from '~components/atoms/linkWithoutPrefetch';
+import root from '~src/lib/routes';
+import useLanguageRoute from '~src/lib/useLanguageRoute';
+
 import { BIBLE_BOOKS } from '../../lib/constants';
 import { PassageNavigationFragment } from './__generated__/passageNavigation';
-import { getChapters } from './passageNavigation.logic';
+import { getBookId, getChapters } from './passageNavigation.logic';
 import styles from './passageNavigation.module.scss';
 
 type Props = {
@@ -14,6 +18,8 @@ export default function PassageNavigation(props: Props): JSX.Element {
 	const [selectedBook, setSelectedBook] = useState<string | null>(
 		BIBLE_BOOKS[0],
 	);
+
+	const languageRoute = useLanguageRoute();
 
 	return (
 		<div className={styles.wrapper}>
@@ -38,7 +44,17 @@ export default function PassageNavigation(props: Props): JSX.Element {
 										const n = title.split(' ').pop()?.padStart(2, '0') ?? '';
 										return (
 											<li key={title} className={styles.chapter}>
-												<button>{n}</button>
+												<Link
+													href={root
+														.lang(languageRoute)
+														.bibles.bookId(
+															getBookId(title, props.audiobibles) ?? '',
+														)
+														.chapterNumber(n)
+														.get()}
+												>
+													{n}
+												</Link>
 											</li>
 										);
 									})}
