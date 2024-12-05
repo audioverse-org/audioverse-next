1585505c3b98bd10a5256625aea26491060c9a5a
Jake Coble <j@kecoble.com>
2024-12-05 11:07:00 -0500

    Add missing components

---
+++ src/components/organisms/bookGrid.tsx
@@ -0,0 +1,40 @@
+import clsx from 'clsx';
+import React from 'react';
+
+import { PassageNavigationFragment } from './__generated__/passageNavigation';
+import ChapterGrid from './chapterGrid';
+import styles from './passageNavigation.module.scss';
+
+type Props = {
+	books: Array<PassageNavigationFragment>;
+	selectedBook: string | number | null;
+	selectBook: (id: string | number | null) => void;
+};
+
+export default function BookGrid({ books, selectedBook, selectBook }: Props) {
+	return (
+		<ul className={clsx(styles.books, 'grid')}>
+			{books.map((book) => {
+				const chapters = book.recordings.nodes;
+
+				return (
+					<>
+						<li
+							key={book.id}
+							className={clsx(styles.book, {
+								active: book.id === selectedBook,
+							})}
+						>
+							<button onClick={() => selectBook(book.id)}>
+								{book.title.replace(' ', '').substring(0, 3)}
+							</button>
+						</li>
+						{book.id === selectedBook && chapters && (
+							<ChapterGrid chapters={chapters} />
+						)}
+					</>
+				);
+			})}
+		</ul>
+	);
+}
