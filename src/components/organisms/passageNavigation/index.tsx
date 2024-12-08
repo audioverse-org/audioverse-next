ef29225678d3fbc60fcf6997cc6a9bf60c80346b
Jake Coble <j@kecoble.com>
2024-12-08 11:48:13 -0500

    Make PassageNavigation self-contained

--- src/components/organisms/passageNavigation/index.tsx
+++ src/components/organisms/passageNavigation/index.tsx
@@ -4,13 +4,27 @@ import { FormattedMessage } from 'react-intl';
 
 import { useLocalStorage } from '~src/lib/hooks/useLocalStorage';
 
-import { PassageNavigationFragment } from './__generated__/index';
+import IconSearch from '~public/img/icons/icon-search.svg';
+import IconDisclosure from '~public/img/icons/icon-disclosure.svg';
 import BookGrid from './bookGrid';
 import BookList from './bookList';
 import styles from './index.module.scss';
+import BibleVersionTypeLockup from '~src/components/molecules/bibleVersionTypeLockup';
+import Dropdown from '~src/components/molecules/dropdown';
+import Button from '~src/components/molecules/button';
+import { getBibleAcronym } from '~src/lib/getBibleAcronym';
+import { GetAudiobibleIndexDataQuery } from '~src/containers/bible/__generated__';
+import IconButton from '~src/components/molecules/iconButton';
+import { BaseColors } from '~src/lib/constants';
+
+export type Version = NonNullable<
+	GetAudiobibleIndexDataQuery['collections']['nodes']
+>[0];
+
+type BookIndex = string | number | null;
 
 type Props = {
-	books: Array<PassageNavigationFragment>;
+	versions: Array<Version>;
 };
 
 // FIXME
@@ -56,64 +70,119 @@ const OT = [
 	'malachi',
 ];
 
-export default function PassageNavigation({ books }: Props): JSX.Element {
-	const [selectedBook, setSelectedBook] = useState<string | number | null>(
-		null,
+export default function PassageNavigation({ versions }: Props): JSX.Element {
+	const [selectedVersion, setSelectedVersion] = useLocalStorage<Version>(
+		'bibleVersion',
+		versions[0],
 	);
+
+	const books = selectedVersion.sequences.nodes || [];
+
+	const [selectedBook, setSelectedBook] = useLocalStorage<BookIndex>(
+		'bibleBook',
+		books[0].id,
+	);
+
 	const [selectedView, setSelectedView] = useLocalStorage<'grid' | 'list'>(
 		'passageNavLayout',
 		'grid',
 	);
 
 	return (
-		<div className={styles.wrapper}>
-			<div className={styles.switch}>
-				<button
-					className={clsx({ active: selectedView === 'grid' })}
-					onClick={() => setSelectedView('grid')}
+		<div className={styles.base}>
+			<div className={styles.hat}>
+				<BibleVersionTypeLockup unpadded />
+				<a className={styles.historyButton} href="https://www.example.com">
+					<FormattedMessage id="bibles__history" defaultMessage="History" />
+				</a>
+				<Dropdown
+					id="booksMenu"
+					trigger={(props) => (
+						<Button
+							type="tertiary"
+							text={getBibleAcronym(selectedVersion.title)}
+							IconRight={IconDisclosure}
+							className={styles.dropdownButton}
+							{...props}
+						/>
+					)}
 				>
-					<FormattedMessage
-						id="passageNavigation__selector-grid"
-						defaultMessage="Grid"
-						description="Switch to grid view"
-					/>
-				</button>
-				<button
-					className={clsx({ active: selectedView === 'list' })}
-					onClick={() => setSelectedView('list')}
-				>
-					<FormattedMessage
-						id="passageNavigation__selector-list"
-						defaultMessage="List"
-						description="Switch to list view"
-					/>
-				</button>
+					{(handleClose) => (
+						<div className={styles.dropdownContainer}>
+							{versions.map((version) => (
+								<p key={version.id}>
+									<Button
+										type="tertiary"
+										onClick={() => {
+											setSelectedVersion(version);
+											handleClose();
+										}}
+										text={getBibleAcronym(version.title)}
+									/>
+								</p>
+							))}
+						</div>
+					)}
+				</Dropdown>
+				<IconButton
+					Icon={IconSearch}
+					onClick={function (): void {
+						throw new Error('Function not implemented.');
+					}}
+					color={BaseColors.WHITE}
+					backgroundColor={BaseColors.DARK}
+				/>
 			</div>
 
-			{selectedView === 'list' ? (
-				<BookList
-					books={books}
-					selectedBook={selectedBook}
-					selectBook={setSelectedBook}
-				/>
-			) : (
-				<>
-					<BookGrid
-						books={books.filter((book) =>
-							OT.includes(book.title.toLocaleLowerCase()),
-						)}
+			<div className={styles.content}>
+				<div className={styles.switch}>
+					<button
+						className={clsx({ active: selectedView === 'grid' })}
+						onClick={() => setSelectedView('grid')}
+					>
+						<FormattedMessage
+							id="passageNavigation__selector-grid"
+							defaultMessage="Grid"
+							description="Switch to grid view"
+						/>
+					</button>
+					<button
+						className={clsx({ active: selectedView === 'list' })}
+						onClick={() => setSelectedView('list')}
+					>
+						<FormattedMessage
+							id="passageNavigation__selector-list"
+							defaultMessage="List"
+							description="Switch to list view"
+						/>
+					</button>
+				</div>
+
+				{selectedView === 'list' ? (
+					<BookList
+						books={books}
 						selectedBook={selectedBook}
 						selectBook={setSelectedBook}
 					/>
-					<BookGrid
-						books={books.filter(
-							(book) => !OT.includes(book.title.toLocaleLowerCase()),
-						)}
-						selectedBook={selectedBook}
-						selectBook={setSelectedBook}
-					/>
-				</>
-			)}
+				) : (
+					<>
+						<BookGrid
+							books={books.filter((book) =>
+								OT.includes(book.title.toLocaleLowerCase()),
+							)}
+							selectedBook={selectedBook}
+							selectBook={setSelectedBook}
+						/>
+						<BookGrid
+							books={books.filter(
+								(book) => !OT.includes(book.title.toLocaleLowerCase()),
+							)}
+							selectedBook={selectedBook}
+							selectBook={setSelectedBook}
+						/>
+					</>
+				)}
+			</div>
 		</div>
 	);
 }
--- src/containers/bible/index.tsx
+++ src/containers/bible/index.tsx
@@ -10,76 +10,23 @@ import Button from '~src/components/molecules/button';
 import Dropdown from '~src/components/molecules/dropdown';
 import IconButton from '~src/components/molecules/iconButton';
 import Tease from '~src/components/molecules/tease';
-import PassageNavigation from '~src/components/organisms/passageNavigation';
+import PassageNavigation, {
+	Version,
+} from '~src/components/organisms/passageNavigation';
 import { getBibleAcronym } from '~src/lib/getBibleAcronym';
 import { useLocalStorage } from '~src/lib/hooks/useLocalStorage';
 
 import { GetAudiobibleIndexDataQuery } from './__generated__';
 import styles from './index.module.scss';
 
-type Version = NonNullable<
-	GetAudiobibleIndexDataQuery['collections']['nodes']
->[0];
-
 export type BibleIndexProps = {
 	data: Array<Version>;
 };
 
 function Bible({ data }: BibleIndexProps): JSX.Element {
-	const [selected, setSelected] = useLocalStorage<Version>(
-		'bibleVersion',
-		data[0],
-	);
-
 	return (
-		<Tease className={styles.base}>
-			<div className={styles.hat}>
-				<BibleVersionTypeLockup unpadded />
-				<a className={styles.historyButton} href="https://www.example.com">
-					<FormattedMessage id="bibles__history" defaultMessage="History" />
-				</a>
-				<Dropdown
-					id="booksMenu"
-					trigger={(props) => (
-						<Button
-							type="tertiary"
-							text={getBibleAcronym(selected.title)}
-							IconRight={IconDisclosure}
-							className={styles.dropdownButton}
-							{...props}
-						/>
-					)}
-				>
-					{(handleClose) => (
-						<div className={styles.dropdownContainer}>
-							{data.map((version) => (
-								<p key={version.id}>
-									<Button
-										type="tertiary"
-										onClick={() => {
-											setSelected(version);
-											handleClose();
-										}}
-										text={getBibleAcronym(version.title)}
-									/>
-								</p>
-							))}
-						</div>
-					)}
-				</Dropdown>
-				<IconButton
-					Icon={IconSearch}
-					onClick={function (): void {
-						throw new Error('Function not implemented.');
-					}}
-					color={BaseColors.WHITE}
-					backgroundColor={BaseColors.DARK}
-				/>
-			</div>
-
-			<div className={styles.content}>
-				<PassageNavigation books={selected.sequences.nodes || []} />
-			</div>
+		<Tease>
+			<PassageNavigation versions={data} />
 		</Tease>
 	);
 }
