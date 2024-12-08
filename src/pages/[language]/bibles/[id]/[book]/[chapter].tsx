036c5e8ac0ee4bb67c989f478ca6be807a9f9ce8
Jake Coble <j@kecoble.com>
2024-12-08 15:38:06 -0500

    Add bible data to chapter detail

--- src/pages/[language]/bibles/[id]/[book]/[chapter].tsx
+++ src/pages/[language]/bibles/[id]/[book]/[chapter].tsx
@@ -8,6 +8,13 @@ import { IBaseProps } from '~containers/base';
 import Book, { BookProps } from '~containers/bible/book';
 import { LANGUAGES, REVALIDATE, REVALIDATE_FAILURE } from '~lib/constants';
 import root from '~lib/routes';
+import { BibleIndexProps } from '~src/containers/bible';
+import {
+	concatBibles,
+	getApiBibles,
+	getFcbhBibles,
+} from '~src/lib/getBibleStaticProps';
+import { getLanguageIdByRoute } from '~src/lib/getLanguageIdByRoute';
 import { getBible } from '~src/services/fcbh/getBible';
 import { getBibleBookChapters } from '~src/services/fcbh/getBibleBookChapters';
 import { getBibles } from '~src/services/fcbh/getBibles';
@@ -17,10 +24,11 @@ export default Book;
 export async function getStaticProps({
 	params,
 }: GetStaticPropsContext<{
+	language: string;
 	id: string;
 	book: string;
 	chapter: string;
-}>): Promise<GetStaticPropsResult<BookProps & IBaseProps>> {
+}>): Promise<GetStaticPropsResult<BookProps & BibleIndexProps & IBaseProps>> {
 	const id = params?.id as string;
 	const book = params?.book as string;
 	const chapterNumber = params?.chapter as string;
@@ -46,6 +54,20 @@ export async function getStaticProps({
 	}
 	const chapters = await getBibleBookChapters(id, bibleBook.testament, book);
 
+	const languageRoute = params?.language || 'en';
+	const languageId = getLanguageIdByRoute(languageRoute);
+
+	const apiBibles = await getApiBibles(languageId);
+
+	if (!apiBibles) {
+		return {
+			notFound: true,
+			revalidate: REVALIDATE_FAILURE,
+		};
+	}
+
+	const fcbhBibles = await getFcbhBibles(languageRoute);
+
 	return {
 		props: {
 			version,
@@ -53,8 +75,9 @@ export async function getStaticProps({
 			chapters,
 			chapterNumber,
 			title: bibleBook.name,
+			data: concatBibles(fcbhBibles, apiBibles),
 		},
-		revalidate: REVALIDATE,
+		revalidate: fcbhBibles ? REVALIDATE : REVALIDATE_FAILURE,
 	};
 }
 
